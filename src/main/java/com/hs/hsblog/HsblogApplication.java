9361997a07749b0e2370302effcf5d8667087fb7
iceswordhs <2760463774@qq.com>
2021-09-01 17:46:53 +0800

    创建项目

---
+++ src/main/java/com/hs/hsblog/HsblogApplication.java
@@ -0,0 +1,13 @@
+package com.hs.hsblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hs/hsblog/HsblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.hsblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
